a096deda1601147e9a9897763b1b63d7725059c3
jhunpaul-prog <jhunpaul331@gmail.com>
2024-09-03 13:10:20 +0800

    second

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Data
+const firstName = "Jhun Paul";
+const lastName = "Ceniza";
+const age = "secret";
+const schoolEmail = "jhma.ceniza.swu@phinmaed.com";
+
+// Routes
+app.get('/firstname', (req, res) => {
+    res.send(firstName.toUpperCase());
+});
+
+app.get('/lastname', (req, res) => {
+    res.send(lastName.toUpperCase());
+});
+
+app.get('/age', (req, res) => {
+    res.send(age.toString());
+});
+
+app.get('/schoolemail', (req, res) => {
+    res.send(schoolEmail);
+});
+
+app.get('/mydata', (req, res) => {
+    res.json({
+        first_name: firstName.toUpperCase(),
+        last_name: lastName.toUpperCase(),
+        age: age,
+        school_email: schoolEmail
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
